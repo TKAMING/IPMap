73772c376af2f30891cbe92638ed8be8730ea55a
TKAMING <tobiaskaruth@gmail.com>
2022-11-21 17:57:29 +0100

    Use api

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 # by Tobias Karuth (TKAMING)
 
 import folium
-import geocoder
+import requests
 from flask import Flask, render_template, redirect, request
+
+# gets ip cordinates
+def ip_cordinates():
+    response = requests.get("http://ip-api.com/json/" + ip).json()
+
+    global lat
+    lat = response['lat']
+    global lon
+    lon = response['lon']
 
 # makes map
 def built_map():
-    g = geocoder.ipinfo(form_ip)
-    userip = g.latlng
-    if userip == []:
-        print("[*] Error IP cordinates not found" + form_ip)
-        userip = [43.7001, -79.4163]
-
-    map = folium.Map(location=userip, zoom_start=12, control_scale=True)
-    folium.CircleMarker(location=userip, radius=50, popup="The radius the IP could be in", circle=folium.Circle(color="black")).add_to(map)
-    folium.Marker(userip, popup='The IP you searched for', icon=folium.Icon(color="black")).add_to(map)
+    map = folium.Map(zoom_start=12, control_scale=True)
+    folium.CircleMarker([lat, lon], radius=50, popup="The radius the IP could be in").add_to(map)
+    folium.Marker([lat, lon], popup='The IP you searched for', icon=folium.Icon(color="black")).add_to(map)
 
     # save and display the map
     map.save('templates/map.html')
-
 
 # flask processes
 app = Flask(__name__)
@@ -27,11 +29,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    ()
     if request.method == "POST":
-        global form_ip
-        form_ip = request.form.get("ip")
-
-        return redirect("/map") 
+        global ip
+        ip = request.form.get("ip")
+        if ip == "":
+            return redirect("/")
+        else:
+            return redirect("/map") 
 
     else:
         return render_template("index.html")
@@ -43,7 +48,6 @@ def map():
     if request.method == "GET":
         built_map()
         return render_template("map.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
